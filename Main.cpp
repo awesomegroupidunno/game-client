060f4acd5c124baa4a94c8f09ac5995502c2f485
adecker5 <adecker5@ycp.edu>
2015-09-20 23:48:43 -0400

    Added a small test program for SDL. The CMakeLists.txt must not be set properly, because it isn't detecting SDL functions.

--- Main.cpp
+++ Main.cpp
@@ -1,14 +1,85 @@
 // Hello World to test CMakeLists.txt
 
 #include <iostream>
+#include <SDL2/SDL.h>
+
+int SDLTest();
 
 int main()
 {
-    std::cout << "Hello Car Combat!\n";
     //TODO: The following is an idea of how Main.cpp should look
     //Draw window / game screen
     //Initialize ServerModel.json
     //Initialize game loop / GameController.cpp
     //Handle exiting game
+
+    SDLTest();
+
     return 0;
+}
+
+int SDLTest()
+{
+    if (SDL_Init(SDL_INIT_VIDEO))
+    {
+        std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("Hello Car Combat!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+    if (window == NULL)
+    {
+        std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (renderer == NULL)
+    {
+        SDL_DestroyWindow(window);
+        std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    std::string imagePath = "/resources/hello.bmp";
+    SDL_Surface *bmp = SDL_LoadBMP(imagePath.c_str());
+    if (bmp == NULL)
+    {
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        std::cout << "SDL_LoadBMP Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, bmp);
+    SDL_FreeSurface(bmp);
+    if (texture == NULL)
+    {
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        std::cout << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    //A sleepy rendering loop, wait for 3 seconds and render and present the screen each time
+    for (int i = 0; i < 3; ++i)
+    {
+        //First clear the renderer
+        SDL_RenderClear(renderer);
+        //Draw the texture
+        SDL_RenderCopy(renderer, texture, NULL, NULL);
+        //Update the screen
+        SDL_RenderPresent(renderer);
+        //Take a quick break after all that hard work
+        SDL_Delay(1000);
+    }
+
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
