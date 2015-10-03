9289917e4713a9aad443f973d14371cc4f01e69c
adecker5 <adecker5@ycp.edu>
2015-10-03 19:52:17 -0400

    made some UdpClient changes in new branch to test out alternate method

--- client/UdpClient.cpp
+++ client/UdpClient.cpp
@@ -1,7 +1,39 @@
 #include "UdpClient.h"
 #include "EncodeDecode.h"
 #include "JsonEncodeDecode.h"
+#include "FakeServer.h"
 
+#include <arpa/inet.h>
+
+int UdpClient::connect_to_server(const char *host, const char *port)
+{
+	int sockfd;
+	ssize_t n;
+	struct sockaddr_in servaddr;
+	char sendline[1000];
+	char recvline[1000];
+
+	sockfd=socket(AF_INET, SOCK_DGRAM, 0);
+
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = inet_addr(host);
+	servaddr.sin_port = htons(32000);
+
+	while (fgets(sendline, 10000, stdin) != NULL)
+	{
+		printf(sendline);
+		sendto(sockfd, sendline, strlen(sendline), 0,
+			   (struct sockaddr *) &servaddr, sizeof(servaddr));
+		n = recvfrom(sockfd, recvline, 10000, 0, NULL, NULL);
+		recvline[n] = 0;
+		fputs(recvline, stdout);
+	}
+
+	return 0;
+}
+
+/*
 int UdpClient::error(const char *msg)
 {
 	perror(msg);
@@ -86,3 +118,4 @@ int UdpClient::close_connection()
 
 	return 0;
 }
+ */
